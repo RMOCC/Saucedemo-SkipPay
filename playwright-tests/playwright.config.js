3b01a1482692a3f12908f6058c4d1a43f284be64
RMOCC <105647063+RMOCC@users.noreply.github.com>
2025-04-03 10:37:52 +0200

    env update

--- playwright-tests/playwright.config.js
+++ playwright-tests/playwright.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   use: {
-    headless: true,
+    headless: false,
     baseURL: 'https://www.saucedemo.com/',
   },
 });
