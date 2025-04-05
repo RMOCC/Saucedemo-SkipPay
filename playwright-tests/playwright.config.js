a52d8857d2a4c3d8995b61ba1da8803160322e1b
RMOCC <105647063+RMOCC@users.noreply.github.com>
2025-04-05 23:23:19 +0200

    Popis změny, např. přidání testu checkoutPage

--- playwright-tests/playwright.config.js
+++ playwright-tests/playwright.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  testDir: './tests',
+  retries: 0,
   use: {
     headless: true,
+    screenshot: 'only-on-failure', // ⬅️ Automatický screenshot při selhání
+    video: 'retain-on-failure',    // ⬅️ Volitelné: zachová video při selhání
+    trace: 'retain-on-failure',    // ⬅️ Volitelné: trace report
   },
-  reporter: [['html', { open: 'never' }]],
 });
